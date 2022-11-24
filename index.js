3cbe4e78e45573d276804b74d8828c0d1155ef2a
eugene <eugeneholenstein@gmail.com>
2022-11-24 20:59:11 +1100

    commit tree 53a62905fc1d117cff8df6140bb3d17dc75980f3

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+
 // code your solution here
+
+function saturdayFun(activity = `roller-skate`){
+    
+    return `This Saturday, I want to ${activity}!`;
+    
+}
+saturdayFun()
+
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+
+function wrapAdjective(flair = '*'){
+    function inner (banana = "special"){
+        console.log(`You are *${banana}*!`);
+        return `You are ${flair}${banana}${flair}!`;
+    }
+    return inner;
+ 
+}
+
+wrapAdjective(`||`);
